02eae768a15c7e0f56dfc4b41de0a53b50ef8863
thanh9256ub <151060906+thanh9256ub@users.noreply.github.com>
2026-08-07 09:27:37 +0700

    update ios app icon

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nguyenvietha/FlutterDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nguyenvietha/FlutterDev/projects/sky_bus"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/nguyenvietha/FlutterDev/projects/sky_bus/macos/Flutter/ephemeral/Packages/.packages/FlutterFramework"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_project\skysoft_bus"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\flutter_project\skysoft_bus\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
